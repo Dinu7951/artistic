c95e148d600e212efd359fac3c1952a9a23077dc
Dinu7951 <dasarhu2005@gmail.com>
2024-08-23 20:09:42 +0530

    edit

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-document.querySelector('.nxt').addEventListener('click',function(){
+var menu =document.querySelector(".menu");
+var ham = document.querySelector(".ham");
+var body = document.querySelector("body");
+var wrong  = document.querySelector(".wrong");
+var head = document.querySelector(".primary-head")
+
+
+menu.addEventListener('click',()=>{
+    menu.style.display = 'none';
+    ham.style.display = 'block';
+    head.style.display = 'none';
+   
+})
+wrong.addEventListener('click',()=>{
+    ham.style.display = 'none';
+    menu.style.display = 'block';
+    head.style.display = '';
     
 })
--- profile.js
+++ profile.js
@@ -1,35 +1,49 @@
-document.querySelector(".bios").addEventListener("click",function(){
-    document.querySelector(".bio").style.display = 'block';
-    document.querySelector(".upload").style.display = 'none';
-    document.querySelector(".password").style.display = 'none';
-    document.querySelector(".social").style.display = 'none';
-    document.querySelector(".achievement").style.display = 'none';
+document.querySelector(".bios").addEventListener("click", function () {
+  document.querySelector(".bio").style.display = "block";
+  document.querySelector(".upload").style.display = "none";
+  document.querySelector(".password").style.display = "none";
+  document.querySelector(".social").style.display = "none";
+  document.querySelector(".achievement").style.display = "none";
 });
-document.querySelector(".uploads").addEventListener("click",function(){
-    document.querySelector(".bio").style.display = 'none';
-    document.querySelector(".upload").style.display = 'block';
-    document.querySelector(".password").style.display = 'none';
-    document.querySelector(".social").style.display = 'none';
-    document.querySelector(".achievement").style.display = 'none';
+document.querySelector(".uploads").addEventListener("click", function () {
+  document.querySelector(".bio").style.display = "none";
+  document.querySelector(".upload").style.display = "block";
+  document.querySelector(".password").style.display = "none";
+  document.querySelector(".social").style.display = "none";
+  document.querySelector(".achievement").style.display = "none";
 });
-document.querySelector(".passwords").addEventListener("click",function(){
-    document.querySelector(".bio").style.display = 'none';
-    document.querySelector(".upload").style.display = 'none';
-    document.querySelector(".password").style.display = 'block';
-    document.querySelector(".social").style.display = 'none';
-    document.querySelector(".achievement").style.display = 'none';
+document.querySelector(".passwords").addEventListener("click", function () {
+  document.querySelector(".bio").style.display = "none";
+  document.querySelector(".upload").style.display = "none";
+  document.querySelector(".password").style.display = "block";
+  document.querySelector(".social").style.display = "none";
+  document.querySelector(".achievement").style.display = "none";
 });
-document.querySelector(".socials").addEventListener("click",function(){
-    document.querySelector(".bio").style.display = 'none';
-    document.querySelector(".upload").style.display = 'none';
-    document.querySelector(".password").style.display = 'none';
-    document.querySelector(".social").style.display = 'block';
-    document.querySelector(".achievement").style.display = 'none';
+document.querySelector(".socials").addEventListener("click", function () {
+  document.querySelector(".bio").style.display = "none";
+  document.querySelector(".upload").style.display = "none";
+  document.querySelector(".password").style.display = "none";
+  document.querySelector(".social").style.display = "block";
+  document.querySelector(".achievement").style.display = "none";
 });
-document.querySelector(".achievement").addEventListener("click",function(){
-    document.querySelector(".bio").style.display = 'block';
-    document.querySelector(".upload").style.display = 'none';
-    document.querySelector(".password").style.display = 'none';
-    document.querySelector(".social").style.display = 'none';
-    document.querySelector(".achievement").style.display = 'block';
+document.querySelector(".achievement").addEventListener("click", function () {
+  document.querySelector(".bio").style.display = "block";
+  document.querySelector(".upload").style.display = "none";
+  document.querySelector(".password").style.display = "none";
+  document.querySelector(".social").style.display = "none";
+  document.querySelector(".achievement").style.display = "block";
+});
+
+var menu = document.querySelector(".menu");
+var ham = document.querySelector(".ham");
+var body = document.querySelector("body");
+var wrong = document.querySelector(".wrong");
+
+menu.addEventListener("click", () => {
+  menu.style.display = "none";
+  ham.style.display = "block";
+});
+wrong.addEventListener("click", () => {
+  ham.style.display = "none";
+  menu.style.display = "block";
 });
